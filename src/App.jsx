19524c448fa1c9ea9f0aa4a09f60869f96e5804d
Mitsuhiro32 <mitsueiwa333@gmail.com>
2024-05-20 22:03:57 -0400

    Axios Pokemon API - estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
 
   const [pokemon, setPokemon] = useState([])
   const URL = useState('https://pokeapi.co/api/v2/pokemon/?limit=10277')
 
-  const fetchPokemon = async () => {
+  /* const fetchPokemon = async () => {
     const response = await fetch(URL)
     if (!response.ok) {
       throw new Error('Fallo al obtener los datos del servidor')
@@ -15,17 +16,35 @@ function App() {
     }
     const data = await response.json()
     setPokemon(data.results)
+  } */
+
+
+  // Usando axios
+  const fetchPokemon = () => {
+    axios.get(URL)
+      .then((response) => {
+        console.log(response.data)
+        setPokemon(response.data.results)
+      }).catch(() => {
+        console.log('Error al obtener los datos del servidor')
+      })
   }
 
   return (
     <>
-      <h1>Pokemon API</h1>
+      <img src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png" alt="Logo de PokeAPI" />
       <button onClick={fetchPokemon}>Fetch Pokemon</button>
-      <ul>
-        {pokemon.map((poke, index) => {
-          return <li key={index}>{poke.name}</li>
+      <div className="pokedex">
+        {pokemon.map((poke) => {
+          const pokeId = poke.url.split('/')[6]
+          return (
+            <div className='card-pokemon' key={pokeId}>
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeId}.png`} alt="Imagen de un Pokémon" />
+              <h2>{poke.name}</h2>
+            </div>
+          )
         })}
-      </ul>
+      </div>
     </>
   )
 }
